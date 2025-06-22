ba8b7b091c7f8423ce18cdbd747c1f08f8531c26
ramyaganeshsan <ramya@indiprotechnologies.com>
2025-06-22 19:56:37 +0530

    deploying

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "../pages/home";
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/Home" replace />} />
+
       <Route path="/Home" element={<HomePage />} />
     </Routes>
   );
